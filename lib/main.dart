fe36a897e801b534cd72a91d3b04e9f8653ffa88
Raafat <raafatm2001@gmail.com>
2025-07-18 01:28:17 +0300

    Add Bloc Observe

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_app/core/helper/custom_bloc_observe.dart';
 import 'App/my_app.dart';
 import 'core/service/StorageLocal/shared_prefs.dart';
 import 'core/service/get_it.dart';
@@ -13,6 +15,7 @@ void main() async{
   await Firebase.initializeApp();
   await PrefsStorage.init();
   setup();
+  Bloc.observer=CustomBlocObserve();
   runApp(const MyApp());
 }
 
